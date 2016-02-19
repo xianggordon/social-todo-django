629e761b5b53f94259b1042c5abfe42c11cb14b2
Gordon Xiang <gordon.xiang@yale.edu>
2016-02-19 01:45:46 +0000

    Progress on splash

--- social_todo/urls.py
+++ social_todo/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^tasks/', include('tasks.urls')),
+    url(r'^tasks/', include('tasks.urls')),  #always use include
     url(r'^admin/', admin.site.urls),
+    url(r'', include('splash.urls')), #root
 ]
 
