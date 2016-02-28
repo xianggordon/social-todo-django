57b4c5e781087bf8ba31d7f5e115ddf891e0b220
xianggordon <gordon.xiang@yale.edu>
2016-02-28 01:13:49 +0000

    working add task

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from tasks.models import Task
 from splash.forms import createTaskForm
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 #tasks/views.py
@@ -13,6 +14,19 @@ def index(request):
 def say_whatsup(request):
     return HttpResponse("Hello, WHAT IS UP?")
     
-def create(request):
-     if request.method == 'POST':
+def newTask(request):
+    if request.method == 'POST':
         form = createTaskForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            newTask = Task(owner=request.user, title=data['title'], description=data['description'])
+            newTask.save()
+            if User.objects.filter(username=data['collaborator1']).exists():
+                newTask.collaborators.add(User.objects.get(username=data['collaborator1']))
+            if User.objects.filter(username=data['collaborator2']).exists():
+                newTask.collaborators.add(User.objects.get(username=data['collaborator1']))
+            if User.objects.filter(username=data['collaborator3']).exists():
+                newTask.collaborators.add(User.objects.get(username=data['collaborator1']))
+                
+    return HttpResponseRedirect('/')  
+
