4b7a100997d2e913df1fddefaec3667c8d0884e9
xianggordon <gordon.xiang@yale.edu>
2016-02-29 03:44:09 +0000

    added CSS and cleaned up things

--- social_todo/settings.py
+++ social_todo/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'tasks.apps.TasksConfig',
     'splash.apps.SplashConfig',
     'django.contrib.admin',
--- social_todo/urls.py
+++ social_todo/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('splash.urls')), #root
     url(r'^tasks/', include('tasks.urls')),  #always use include
-    url(r'^polls/', include('polls.urls')), #testing using polls
     url(r'^admin/', admin.site.urls),
 ]
 
